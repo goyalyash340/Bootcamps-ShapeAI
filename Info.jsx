32fb64669643aeb536c05b7049a6b3ce7a2715e5
Yash Goyal <72781183+goyalyash340@users.noreply.github.com>
2021-06-27 18:13:39 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div className="note">
+      <h1>Basic Front End Web Dev with JavaScript and React.js</h1>
+        <p>7 Days, Certified, Free Javascript and React.js</p>
+        <p>Here we bring our another free live BOOTCAMP with a hands on project at the end;</p>
+        <p>Basic Front end Development with Javascript and React.js - </p>
+        <p>The Foundation of Web Development 🖥</p>
+        <p>Prerequisite: None</p>
+        <p>Duration - 7 days</p>
+    </div>
+  );
+}
+
+export default Info;
---
+++ Info2.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div className="note">
+      <h1>Python and Data Analytics Bootcamp</h1>
+        <p>7 Days, Certified, Free Python and Data Analytics</p>
+        <p>Here we bring our another free live BOOTCAMP with a hands on project at the end;</p>
+        <p>Python and Data Analytics -</p>
+        <p>The Foundation of Data Science 🖥</p>
+        <p>Prerequisite: None</p>
+        <p>Duration - 7 days</p>
+    </div>
+  );
+}
+
+export default Info;
